01a0139ef846057a9b51981f2e79afd67fc4170a
lyf <lyf950412@126.com>
2020-04-21 17:32:24 +0800

    增加 岛屿数量 题解

---
+++ leetCodeArithmetic/src/main/java/github/lyf/middle/NumIslands.java
@@ -0,0 +1,63 @@
+package github.lyf.middle;
+
+/**
+ * @author lyf
+ */
+public class NumIslands {
+    /**
+     *给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+     *
+     * 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+     *
+     * 此外，你可以假设该网格的四条边均被水包围。
+     *
+     * 示例 1:
+     *
+     * 输入:
+     * 11110
+     * 11010
+     * 11000
+     * 00000
+     * 输出: 1
+     * 示例 2:
+     *
+     * 输入:
+     * 11000
+     * 11000
+     * 00100
+     * 00011
+     * 输出: 3
+     * 解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        /**
+         * 思路：遍历岛这个二维数组，如果当前数为1，则进入感染函数并将岛个数+1
+         * 感染函数：其实就是一个递归标注的过程，它会将所有相连的1都标注成2。
+         * 为什么要标注？这样就避免了遍历过程中的重复计数的情况，一个岛所有的1都变成了2后，遍历的时候就不会重复遍历了
+         */
+        int islandNum = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    infect(grid, i, j);
+                    islandNum++;
+                }
+            }
+        }
+        return islandNum;
+    }
+    //感染函数
+    public void infect(char[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length ||
+                j < 0 || j >= grid[0].length || grid[i][j] != '1'){
+            return;
+        }
+        grid[i][j] = '2';
+        infect(grid, i + 1, j);
+        infect(grid, i - 1, j);
+        infect(grid, i, j + 1);
+        infect(grid, i, j - 1);
+    }
+}
